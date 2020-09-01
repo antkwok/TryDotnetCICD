7073dd566cd003c7434a940ef1ef76462e9b469b
Anthony Kwok <anthonykwok83@gmail.com>
2020-09-01 20:23:32 +0800

    init commit

---
+++ TryDotnetCICD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryDotnetCICD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
